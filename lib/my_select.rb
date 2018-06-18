49415308663dae10145d3256aefdfd63d24d7557
Rebecca Govert <becci.govert@gmail.com>
2018-06-18 20:05:47 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
  if block_given?
    i = 0 
    while i < collection.length 
-     results_array.push yield collection[i] 
+    #results_array.push yield collection[i] 
       i += 1 
     end
    # if collection[i] == TRUE
@@ -19,6 +19,6 @@ numbers = [1,2,3,4,5]
 
 
 my_select(numbers) do |num|
-  num if num % 2 == 0
+  num % 2 == 0
 end
 
